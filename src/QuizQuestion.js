59ed03577452cdde031e23283481916fead3d777
Dawit Admassu <dadmassu1920@gmail.com>
2020-02-07 13:59:44 +0100

    Add error message when submitting wrong answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
-  handleClick(buttonText) {
-    console.log(this.props.quiz_question.answer === buttonText)
-    if(buttonText === this.props.quiz_question.answer) {
-      this.props.showNextQuestionHandler('test');
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       incorrectAnswer: false
     }
   }
+  
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler('test');
+      this.setState({
+        incorrectAnswer: false
+      })
+    } else {
+      this.setState({
+        incorrectAnswer: true
+      })
+    }
+  }
+
   render() {
-    const options = this.props.quiz_question.answer_options;
-    console.log(this.props.quiz_question);
     return (
       <main>
         <section>
@@ -18,9 +31,15 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            {this.props.quiz_question.answer_options.map((option, index) => <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={option}/>)}
+            {this.props.quiz_question.answer_options
+              .map((option, index) =>
+                <QuizQuestionButton 
+                clickHandler={this.handleClick.bind(this)} 
+                key={index} 
+                button_text={option} />)}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
       </main>
     )
   }
